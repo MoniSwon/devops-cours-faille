4b644b6633bf59e0f80fede2e3caf45d09897f78
Monica W <77145289+MoniSwon@users.noreply.github.com>
2023-06-02 14:31:57 +0200

    Update write.js

--- write.js
+++ write.js
@@ -1,5 +1,6 @@
-let titleElement = document.getElementById("title");
-let name = new URLSearchParams(window.location.search).get("name");
-if (name !== null) {
-  titleElement.innerHTML = `Hello ${name}`;
-}
+const h1 = document.createElement('h1')
+const params = new URLSearchParams(window.location.search)
+const mister = params.get('name')
+
+h1.innerHTML = `Hello ${mister}`
+document.querySelector('body').appendChild(h1)
